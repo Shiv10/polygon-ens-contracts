b814c622e806b365f73aff909eb94928ff41263f
Shiv10 <shivanshsharma2012@gmail.com>
2022-02-21 14:17:58 +0530

    feat: add payments

--- scripts/run.js
+++ scripts/run.js
@@ -8,18 +8,23 @@ const main = async () => {
     console.log(`Contract deployed to address: ${domainContract.address}`);
     console.log(`Contract is deployed by: ${owner.address}`);
 
-    let txn = await domainContract.register("doom", { value: hre.ethers.utils.parseEther('0.1')});
+    let txn = await domainContract.register("doom", { value: hre.ethers.utils.parseEther('100')});
     await txn.wait();
-
-    const domainOwner = await domainContract.getAddress("doom");
-    console.log(`Owner of domain doom is: ${domainOwner}`);
 
     // txn = await domainContract.setRecord("doom", "I am doctor doom.");
     // await txn.wait();
     // const record = await domainContract.getRecord("doom");
     // console.log(`Got records for domain : doom => ${record}`);
+    let ownerBalance = await hre.ethers.provider.getBalance(owner.address);
+    console.log("Owner wallet balance is: ", hre.ethers.utils.formatEther(ownerBalance));
+    let balance = await hre.ethers.provider.getBalance(domainContract.address);
+    console.log(`Contract balance is: ${hre.ethers.utils.formatEther(balance)}`);
 
-    const balance = await hre.ethers.provider.getBalance(domainContract.address);
+    txn = await domainContract.withdraw();
+    await txn.wait();
+    ownerBalance = await hre.ethers.provider.getBalance(owner.address);
+    console.log("Owner wallet balance is: ", hre.ethers.utils.formatEther(ownerBalance));
+    balance = await hre.ethers.provider.getBalance(domainContract.address);
     console.log(`Contract balance is: ${hre.ethers.utils.formatEther(balance)}`);
 
 }
